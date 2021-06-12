d0a1246430e0946aec1f7ea0d8615ea78dbafe69
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 16:49:41 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+
+        Level myVar = Level.MEDIUM;
+
+        switch(myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+        
+
+    }
+
+}
